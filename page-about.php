b13987f2c92c854fd39963cee2ff3ad246c0a6df
smaq92 <145607578+smaq92@users.noreply.github.com>
2025-03-27 21:16:11 -0600

    add notes and edit about us page id

--- page-about.php
+++ page-about.php
@@ -20,7 +20,7 @@ get_header();
 	<?php
 	while (have_posts()) :
 		the_post(); ?>
-		<section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<section id="about-page" <?php post_class(); ?>>
 			<header class="entry-header">
 				<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 			</header><!-- .entry-header -->
